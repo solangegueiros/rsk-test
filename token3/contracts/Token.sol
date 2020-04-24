6471715ee4d8f7c7c7bc6cccf0f13699075707ce
Solange Gueiros <solangegueiros@gmail.com>
2020-04-24 11:54:35 -0300

    02

--- token3/contracts/Token.sol
+++ token3/contracts/Token.sol
@@ -3,7 +3,7 @@ pragma solidity 0.5.2;
 import '@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol';
 
 contract Token is ERC20Mintable{
-       string public name = "Meu RSK token";
-       string public symbol = "MRT";
+       string public name = "My RSK token 03";
+       string public symbol = "MRT03";
        uint8 public decimals = 2;
 }
